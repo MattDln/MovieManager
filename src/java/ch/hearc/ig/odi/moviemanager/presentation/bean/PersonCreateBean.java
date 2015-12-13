24f7827e06c757d691f0070a1e847b108fdf29f5
DeillonM <DeillonM@DeillonM-PC>
2015-12-13 17:37:19 +0100

    Matthieu Deillon ---------------------------- Ajout création de la personne

---
+++ src/java/ch/hearc/ig/odi/moviemanager/presentation/bean/PersonCreateBean.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.moviemanager.presentation.bean;
+
+import ch.hearc.ig.odi.moviemanager.buisness.Person;
+import ch.hearc.ig.odi.moviemanager.service.Services;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Bean lié à la page createPerson.xhtml, qui permet de créer une nouvelle personne ou de la modifier
+ * @author DeillonM
+ */
+@Named(value="personCreateBean")
+@RequestScoped
+public class PersonCreateBean implements Serializable{
+    
+    @Inject Services services;
+    private Person person;
+    
+    public PersonCreateBean(){
+        
+    }
+    
+    public String editPerson (Person pers){
+        if(pers !=null){
+            person = pers;
+        }else{
+            person=new Person();
+        }
+        return "edit";
+    } 
+
+    /**
+     * Méthode qui permet de sauvegarder une personne saisi.
+     * @return "success" si l'enregistrement se passe bien, "errorDuplicate" si le numéro de la personne est déjà utilisé.
+     */
+    public String saveCustomer(){
+        try{
+            services.sa(person.getId(), person.getFirstName(), person.getLastName());
+            return "success";
+        }catch(DuplicateElementException dee){
+            return "errorDuplicate";
+        }
+        
+    }
+    
+    public Person getPerson(){
+        return person;
+    }
+    
+    public void setPerson(Person pers){
+        this.person = pers;
+    }   
+}
